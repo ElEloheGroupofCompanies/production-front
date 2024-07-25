258c99804e809a788a3ee0ae0d02e28387f55efb
ElElohe <elelohegroup2019@gmail.com>
2024-07-25 11:10:46 +0800

    changes the api instances

--- src/utilities/http.js
+++ src/utilities/http.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 function useApi(token = null) {
   return axios.create({
-    baseURL: "https://db-eebbqproduction.onrender.com/api",
+    baseURL: "https://production-services.onrender.com/api",
     headers: {
       Accept: "application/json",
       Authorization: `Bearer ${token}`,
